a9d7dd51f560811e6796209bed0b41478e55be01
Sean <sean_css@163.com>
2017-02-17 18:17:15 +0800

    none

--- app/src/main/java/com/ipc/sean/view/MainActivity.java
+++ app/src/main/java/com/ipc/sean/view/MainActivity.java
@@ -1,15 +1,54 @@
 package com.ipc.sean.view;
 
+import android.annotation.SuppressLint;
+import android.os.Handler;
+import android.os.Message;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 import android.view.WindowManager;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    TestButton testButton;
+
+    private static final int MESSAGE_SCROLL_TO = 1;
+    private static final int FRAME_COUNT = 30;
+    private static final int DELAYED_TIME = 33;
+
+    private int mCount = 0;
+
+    @SuppressLint("HandlerLeak")
+    private Handler mHandler = new Handler(){
+        @Override
+        public void handleMessage(Message msg) {
+            switch (msg.what){
+                case MESSAGE_SCROLL_TO:
+                    mCount++;
+                    if (mCount <= FRAME_COUNT){
+                        float fraction = mCount / (float)FRAME_COUNT;
+                        int scrollX = (int)(fraction * 100);
+                        testButton.scrollTo(scrollX,0);
+                        mHandler.sendEmptyMessageDelayed(MESSAGE_SCROLL_TO,DELAYED_TIME);
+                    }
+                    break;
+            }
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+//        this.getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.activity_main);
+        testButton = (TestButton) findViewById(R.id.button2);
+        testButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "test", Toast.LENGTH_SHORT).show();
+            }
+        });
+        mHandler.sendEmptyMessageDelayed(MESSAGE_SCROLL_TO, DELAYED_TIME);
     }
 }
--- app/src/main/java/com/ipc/sean/view/TestButton.java
+++ app/src/main/java/com/ipc/sean/view/TestButton.java
@@ -5,35 +5,39 @@ import android.content.Context;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.MotionEvent;
-import android.view.View;
+import android.view.VelocityTracker;
 import android.view.ViewConfiguration;
+import android.widget.Button;
+import android.widget.Toast;
 
-import com.nineoldandroids.view.ViewHelper;
 
 /**
  * Created by css on 17/2/16.
  */
 
-public class TestButton extends View {
+public class TestButton extends Button {
     private static final String TAG = "TestButton";
     private int mScaledTouchSlop;
     // 分别记录上次滑动的坐标
     private int mLastX = 100;//初始化view的x坐标点
     private int mLastY = 100;//初始化view的y坐标点
-
+    private Context context;
 
 
     public TestButton(Context context) {
         this(context, null);
+        this.context = context;
     }
 
     public TestButton(Context context, AttributeSet attrs) {
         super(context, attrs);
+        this.context = context;
         init();
     }
 
     public TestButton(Context context, AttributeSet attrs, int defStyle) {
         super(context, attrs, defStyle);
+        this.context = context;
         init();
     }
 
@@ -49,27 +53,27 @@ public class TestButton extends View {
         //getRawX 获取相对于手机屏幕上左上角的x坐标，手指当前的坐标
         //getRawX 获取相对于手机屏幕上左上角的y坐标，手指当前的坐标
         int x = (int) event.getRawX();
+//        int x = (int) event.getX();
         int y = (int) event.getRawY();
-        Log.d(TAG, "getRawX: "+event.getRawX());
-        Log.d(TAG, "getX: "+this.getX());
-        Log.d(TAG, "getTranslationX: "+this.getTranslationX());
-
-        Log.d(TAG, "getRawY: "+event.getRawY());
-        Log.d(TAG, "getY: "+this.getY());
-        Log.d(TAG, "getTranslationY: "+this.getTranslationY());
+//        int y = (int) event.getY();
+        VelocityTracker velo = VelocityTracker.obtain();
+        velo.addMovement(event);
+        velo.computeCurrentVelocity(100);
         switch (event.getAction()) {
             case MotionEvent.ACTION_DOWN: {//手指刚接触屏幕
                 break;
             }
             case MotionEvent.ACTION_MOVE: {//手指在屏幕上移动
+                //x是当前位置相对于手机屏幕的x坐标，mLastX是初始的view的左上角x坐标,得到
                 int deltaX = x - mLastX;
+                //y是当前位置相对于手机屏幕的y坐标，mLastY是初始的view的左上角Y坐标
                 int deltaY = y - mLastY;
                 //translationX相对于父容器的x偏移量
                 //translationX相对于父容器的y偏移量
                 int translationX = (int) this.getTranslationX() + deltaX;
                 int translationY = (int) this.getTranslationY() + deltaY;
-                this.setTranslationX( translationX);
-                this.setTranslationY( translationY);
+                this.setTranslationX(translationX);
+                this.setTranslationY(translationY);
                 break;
             }
             case MotionEvent.ACTION_UP: {//手指从屏幕上松开的一瞬间
